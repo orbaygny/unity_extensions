11db56d718267b07845559fdab19093eeeb2bd3e
orbaygny <orbay_1997@hotmail.com>
2026-04-01 18:59:28 +0300

    Shuffle function added

--- ListExtensions.cs
+++ ListExtensions.cs
@@ -23,4 +23,13 @@ public static class ListExtensions
             result.Add(list[i]);
         }
     }
+
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        for (int i = list.Count - 1; i > 0; i--)
+        {
+            int j = Random.Range(0, i + 1);
+            (list[i], list[j]) = (list[j], list[i]);
+        }
+    }
 }
